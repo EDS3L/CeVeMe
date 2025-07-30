de932e2b9ff001a080d7808940cdcde8c87f9b1d
BartekB <bartek011229@gmail.com>
2025-07-30 23:04:16 +0200

    front

--- Frontend/ceveme/src/features/home/components/SectionOneHome.jsx
+++ Frontend/ceveme/src/features/home/components/SectionOneHome.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import HomeNavBar from "../components/HomeNavBar";
 
 function SectionOneHome() {
-  return (
-    <>
-      <div className="bg-manilla h-screen mx-auto flex items-center justify-center">
-        <div className="w-[95%] h-[80%] border-2 border-black"></div>
-      </div>
-    </>
-  );
+	return (
+		<div className="h-screen flex flex-col">
+			<HomeNavBar />
+			<div className="flex-1 bg-manilla flex items-center justify-center">
+				<div className="w-5/6 h-8/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 bg-cloudlight border-2 rounded-4xl border-black flex justify-center items-center">
+					<p className="text-center">Witaj</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SectionOneHome;
--- Frontend/ceveme/src/features/home/pages/Home.jsx
+++ Frontend/ceveme/src/features/home/pages/Home.jsx
@@ -1,13 +1,30 @@
-import { Link } from 'react-router-dom';
-import SectionOneHome from '../components/SectionOneHome';
-import HomeNavBar from '../components/HomeNavBar';
+import { Link } from "react-router-dom";
+import SectionOneHome from "../components/SectionOneHome";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <HomeNavBar />
-      <SectionOneHome />
-      <div className="h-screen w-screen bg-black">s</div>
-    </div>
-  );
+	const [showNavigation, setShowNavigation] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setShowNavigation(window.scrollY > 150);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	return (
+		<div>
+			<SectionOneHome />
+			<div className="h-screen w-screen bg-black">s</div>
+			{showNavigation && (
+				<div className="fixed top-10 left-1/2 transform -translate-x-1/2 z-50">
+					<div className="bg-red-600 text-white px-6 py-3 rounded-xl shadow-lg hover:bg-red-700 transition">
+						Danger
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
