2ab0af3ece7ab47bc4055ae481dc5e02b201edc1
BartekB <bartek011229@gmail.com>
2025-10-27 21:24:09 +0100

    poprawa UI oferty pracy, limit slow na ofercie - zabezpieczenie prawne, dodanie brakujacych danych z backendu do jobModal

--- Frontend/ceveme/src/features/joboffers/components/jobs/SkeletonCard.jsx
+++ Frontend/ceveme/src/features/joboffers/components/jobs/SkeletonCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Card from '../ui/Card';
 
 export default function SkeletonCard() {
-  return (
-    <Card className="p-5 animate-pulse">
-      <div className="h-5 w-2/3 bg-ivorymedium rounded"></div>
-      <div className="mt-3 h-4 w-1/3 bg-ivorymedium rounded"></div>
-      <div className="mt-4 flex gap-2">
-        <div className="h-6 w-24 bg-ivorymedium rounded-full"></div>
-        <div className="h-6 w-28 bg-ivorymedium rounded-full"></div>
-        <div className="h-6 w-32 bg-ivorymedium rounded-full"></div>
-      </div>
-      <div className="mt-4 h-4 w-1/2 bg-ivorymedium rounded"></div>
-    </Card>
-  );
+	return (
+		<Card className='p-5 animate-pulse'>
+			<div className='h-5 w-2/3 bg-ivorymedium rounded'></div>
+			<div className='mt-3 h-4 w-1/3 bg-ivorymedium rounded'></div>
+			<div className='mt-4 flex gap-2'>
+				<div className='h-6 w-24 bg-ivorymedium rounded-full'></div>
+				<div className='h-6 w-28 bg-ivorymedium rounded-full'></div>
+				<div className='h-6 w-32 bg-ivorymedium rounded-full'></div>
+			</div>
+			<div className='mt-4 h-4 w-1/2 bg-ivorymedium rounded'></div>
+		</Card>
+	);
 }
